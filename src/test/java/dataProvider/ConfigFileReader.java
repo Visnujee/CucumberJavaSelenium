57cf5f8d0a645ece56662410ad197036b2d8beea
Visnujee J <visnujee91@gmail.com>
2025-04-17 22:09:44 +0530

    configfile reader implementation

--- src/test/java/dataProvider/ConfigFileReader.java
+++ src/test/java/dataProvider/ConfigFileReader.java
@@ -4,6 +4,21 @@ import java.io.*;
 import java.util.Properties;
 
 public class ConfigFileReader  {
+    private static Properties properties;
+    private final String propertyFilePath = System.getProperty("configs/configuration.properties");
 
+    public ConfigFileReader() throws IOException {
+        BufferedReader reader;
+        reader = new BufferedReader(new FileReader(propertyFilePath));
+        properties = new Properties();
+        properties.load(reader);
+
+    }
+
+    public static String getApplicationUrl() {
+        String url = properties.getProperty("url");
+        if(url != null) return url;
+        else throw new RuntimeException("url not specified in the Configuration.properties file.");
+    }
 
 }
--- src/test/java/pages/homepage.java
+++ src/test/java/pages/homepage.java
@@ -1,29 +1,28 @@
 package pages;
 
 import dataProvider.ConfigFileReader;
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.PageFactory;
 import utility.BrowserDriver;
-
 import java.io.IOException;
 import java.time.Duration;
-import java.util.Properties;
-
-import static utility.BrowserDriver.driver;
 
 
 public class homepage extends BrowserDriver{
-        public void navigateHomepage() throws InterruptedException, IOException {
 
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-          driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
-
+    public void navigateHomepage() throws InterruptedException, IOException {
+        ConfigFileReader configFileReader = new ConfigFileReader();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        System.out.println("config======================>");
+        System.out.println("config======================>" + ConfigFileReader.getApplicationUrl());
+//        driver.get(configFileReader.getApplicationUrl());
+        //        driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
     }
+
     public void forgotLink()throws InterruptedException {
         driver.findElement(By.xpath("//div[@class =\"forgot-link\"]")).click();
-
     }
 
         public void forgotPasswordPage()throws InterruptedException{
